7a700ac9c9219ee99124d89d5efcb64cca9de59f
mdatsev <martin.datsev.p@gmail.com>
2017-01-17 22:57:21 +0200

    Set up a connection with socket.io

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const path = require('path');
 
-app.get('/', (req, res) => {
-    res.send('Hi');
-});
+app.use(express.static('public'));
 
 http.listen(3000, () => {
     console.log('listening...');
+});
+
+io.on('connection', function(socket) {
+    console.log(`New connection: ${socket.id}`);
+    socket.on('disconnect', function() {
+        console.log(`Disconnected: ${socket.id}`);
+    });
 });
