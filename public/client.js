da0aeac057c74f75aaeac1e75cf300c569703748
mdatsev <martin.datsev.p@gmail.com>
2017-01-17 23:37:24 +0200

    added p5.js

--- public/client.js
+++ public/client.js
@@ -1,13 +1,8 @@
-var socket = io();
-document.onkeydown = (event) => {
-    console.log('hi');
-    if (event.keyCode === 38) {
-        socket.emit('key press', 'UP');
-    } else if (event.keyCode === 40) {
-        socket.emit('key press', 'DOWN');
-    } else if (event.keyCode === 37) {
-        socket.emit('key press', 'LEFT');
-    } else if (event.keyCode === 39) {
-        socket.emit('key press', 'RIGHT');
-    }
-};
+function setup() {
+    createCanvas(300, 660);
+    background(55);
+}
+
+function draw() {
+    rect(50, 50, 80, 80);
+}
