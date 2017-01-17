317d0bbeabc640b92e7f64e9b0dd740ce993f530
mdatsev <martin.datsev.p@gmail.com>
2017-01-18 00:27:37 +0200

    created canvas elements

--- public/client.js
+++ public/client.js
@@ -1,8 +1,26 @@
+const wellWidth = 200,
+    wellHeight = 440,
+    sidebarSize = 75,
+    ground = 10,
+    round = 0,
+    width = 2 * sidebarSize + wellWidth,
+    height = sidebarSize + wellHeight + ground,
+    wellColor = '#33343a',
+    sidebarColor = '#52596d',
+    backroundColor = '#52596d';
+
 function setup() {
-    createCanvas(300, 660);
-    background(55);
+    createCanvas(width + 1, height + 1);
+    background(backroundColor);
+    noStroke();
+    fill(wellColor);
+    rect(sidebarSize, sidebarSize, wellWidth, wellHeight, round / 2);
+    fill(backroundColor);
+    rect(0, 0, width, sidebarSize, round);
+    rect(0, sidebarSize, sidebarSize, height - sidebarSize, round);
+    rect(sidebarSize + wellWidth, sidebarSize, sidebarSize, height - sidebarSize, round);
 }
 
 function draw() {
-    rect(50, 50, 80, 80);
+
 }
